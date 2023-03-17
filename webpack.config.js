763f72140fb6a80666c12d58055e64d282919e6e
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-16 20:03:47 -0700

    Add viewport meta tag to HTMLWebpackPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ const config = {
   plugins: [
     new HtmlWebpackPlugin({
       templateContent: ({ htmlWebpackPlugin }) =>
-        '<!DOCTYPE html><html><head><meta charset="utf-8"><title>' +
+        '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width, initial-scale=1"><title>' +
         htmlWebpackPlugin.options.title +
         '</title></head><body><div id="app"></div></body></html>',
       filename: 'index.html',
