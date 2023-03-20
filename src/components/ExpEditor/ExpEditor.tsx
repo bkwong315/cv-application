daf0c7ad2e303e75b7efa50a2fdfde35575b3b48
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-20 16:35:38 -0700

    Add and link ExpItem to ExpEditor

--- src/components/ExpEditor/ExpEditor.tsx
+++ src/components/ExpEditor/ExpEditor.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 
+import ExpItem from './ExpItem';
 import './ExpEditor.scss';
 
 class ExpEditor extends React.Component {
   render() {
-    return <div className='exp-editor'>Edit</div>;
+    return (
+      <div className='exp-editor'>
+        <ExpItem />
+      </div>
+    );
   }
 }
 
---
+++ src/components/ExpEditor/ExpItem.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+import './ExpItem.scss';
+
+class ExpItem extends React.Component {
+  render() {
+    return <div className='exp-item'>Item</div>;
+  }
+}
+
+export default ExpItem;
