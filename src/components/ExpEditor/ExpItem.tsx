e25e9b58b7d9931ee6c2f341f4e7e172f06d67ce
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-20 16:40:26 -0700

    Add inputs to WorkExp

--- src/components/ExpEditor/ExpItem.tsx
+++ src/components/ExpEditor/ExpItem.tsx
@@ -1,10 +1,39 @@
 import * as React from 'react';
 
+import LabeledInput from '../LabeledInput/LabeledInput';
 import './ExpItem.scss';
 
 class ExpItem extends React.Component {
   render() {
-    return <div className='exp-item'>Item</div>;
+    return (
+      <div className='exp-item'>
+        <LabeledInput
+          inputId='job-title'
+          inputName='Job Title'
+          onChangeHandler={() => {}}
+        />
+        <LabeledInput
+          inputId='company'
+          inputName='Company'
+          onChangeHandler={() => {}}
+        />
+        <LabeledInput
+          inputId='start-date'
+          inputName='Start Date'
+          onChangeHandler={() => {}}
+        />
+        <LabeledInput
+          inputId='end-date'
+          inputName='End Date'
+          onChangeHandler={() => {}}
+        />
+        <LabeledInput
+          inputId='job-duties'
+          inputName='Duties'
+          onChangeHandler={() => {}}
+        />
+      </div>
+    );
   }
 }
 
