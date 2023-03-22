c606377fd633902004c9a32fd1c39b7589be0725
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-21 21:15:41 -0700

    Add handlers to EducationEditor

--- src/components/EducationEditor/EducationEditor.tsx
+++ src/components/EducationEditor/EducationEditor.tsx
@@ -1,20 +1,36 @@
 import * as React from 'react';
 
+import Education from '../../interfaces/Education';
+import EduItem from './EduItem';
 import './EducationEditor.scss';
 
-class EducationEditor extends React.Component {
+interface EducationEditorProps {
+  education: { [key: string]: Education };
+  addHandler: () => void;
+  deleteHandler: (id: string) => void;
+  onChangeHandler: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    field: string,
+    id: string
+  ) => void;
+}
+
+class EducationEditor extends React.Component<EducationEditorProps> {
   render() {
+    const { education, addHandler, deleteHandler, onChangeHandler } =
+      this.props;
+
     return (
-      <div className='education-editor'>
+      <div className='edu-editor'>
         {Object.keys(education).map((key) => (
-          <ExpItem
+          <EduItem
             key={key}
             id={key}
-            handleDelete={handleDeleteEducation}
-            onChangeHandler={handleEducationChange}
+            handleDelete={deleteHandler}
+            onChangeHandler={onChangeHandler}
           />
         ))}
-        <button className='add-exp-btn' onClick={handleAddEducation}>
+        <button className='add-edu-btn' onClick={addHandler}>
           Add Education
         </button>
       </div>
