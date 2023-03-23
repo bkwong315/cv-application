f2e118610120ce0163b18b28fad1664e4b34d629
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-22 19:09:49 -0700

    Add personalInfo to cv-preview

--- src/components/CVPreview/CVPreview.tsx
+++ src/components/CVPreview/CVPreview.tsx
@@ -1,10 +1,29 @@
 import * as React from 'react';
 
+import UserInfo from '../../interfaces/UserInfo';
 import './CVPreview.scss';
 
-class CVPreview extends React.Component {
+class CVPreview extends React.Component<UserInfo> {
   render() {
-    return <div className='cv-preview'>CV PREVIEW</div>;
+    const { personalInfo, experience, education } = this.props;
+    return (
+      <div className='cv-preview'>
+        <header>
+          {(personalInfo.firstName !== '' || personalInfo.lastName !== '') && (
+            <h2>{`${personalInfo.firstName} ${personalInfo.lastName}`}</h2>
+          )}
+          {personalInfo.address !== '' && (
+            <p className='address'>Address: {personalInfo.address}</p>
+          )}
+          {personalInfo.email !== '' && (
+            <p className='email'>Email: {personalInfo.email}</p>
+          )}
+          {personalInfo.telephone !== '' && (
+            <p className='telephone'>Phone: {personalInfo.telephone}</p>
+          )}
+        </header>
+      </div>
+    );
   }
 }
 
