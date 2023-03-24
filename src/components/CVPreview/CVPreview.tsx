08b51c45557c519ca9b7beeee2209671a4fb289b
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-23 18:28:26 -0700

    Add wrappers and class names to preview items

--- src/components/CVPreview/CVPreview.tsx
+++ src/components/CVPreview/CVPreview.tsx
@@ -25,20 +25,23 @@ class CVPreview extends React.Component<UserInfo> {
         <div className='experience-container'>
           {Object.keys(experience).map((key) => {
             return (
-              <div key={`experience-${key}`}>
-                {experience[key].jobTitle}
-                {experience[key].company}
-                {experience[key].startDate}
-                {experience[key].endDate}
-                {experience[key].duties}
+              <div key={`experience-${key}`} className={'exp-display-item'}>
+                <p className='job-title-display'>{experience[key].jobTitle}</p>
+                <p className='company-name-display'>
+                  {experience[key].company}
+                </p>
+                <p className='exp-date-display'>
+                  {experience[key].startDate} - {experience[key].endDate}
+                </p>
+                <p className='duties-display'>{experience[key].duties}</p>
               </div>
             );
           })}
         </div>
-        <div className='education-container'>
+        <div className='education-container edu-display-item'>
           {Object.keys(education).map((key) => {
             return (
-              <div key={`education-${key}`}>
+              <div key={`education-${key}`} className={'edu-display-item'}>
                 {education[key].institution}
                 {education[key].degree}
                 {education[key].subject}
