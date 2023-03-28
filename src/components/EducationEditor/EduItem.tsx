8347364364e9c9191a7105c215499ba6b096852c
bkwong315 <50161878+bkwong315@users.noreply.github.com>
2023-03-28 16:21:58 -0700

    Update EduItem to functional component

--- src/components/EducationEditor/EduItem.tsx
+++ src/components/EducationEditor/EduItem.tsx
@@ -15,56 +15,54 @@ interface EduItemProps {
   ) => void;
 }
 
-class EduItem extends React.Component<EduItemProps> {
-  render() {
-    const { id, defaultValues, handleDelete, onChangeHandler } = this.props;
-    return (
-      <div className='edu-item'>
-        <LabeledInput
-          id={id}
-          inputId='institution'
-          inputName='Institution'
-          defaultValue={defaultValues.institution}
-          onChangeHandler={onChangeHandler}
-        />
-        <LabeledInput
-          id={id}
-          inputId='degree'
-          inputName='Degree'
-          defaultValue={defaultValues.degree}
-          onChangeHandler={onChangeHandler}
-        />
-        <LabeledInput
-          id={id}
-          inputId='subject'
-          inputName='Subject'
-          defaultValue={defaultValues.subject}
-          onChangeHandler={onChangeHandler}
-        />
-        <LabeledInput
-          id={id}
-          inputId='start-date'
-          inputName='Start Date'
-          inputType='date'
-          defaultValue={defaultValues.startDate}
-          onChangeHandler={onChangeHandler}
-        />
-        <LabeledInput
-          id={id}
-          inputId='end-date'
-          inputName='End Date'
-          inputType='date'
-          defaultValue={defaultValues.endDate}
-          onChangeHandler={onChangeHandler}
-        />
-        <button
-          className='del-btn span-full-width'
-          onClick={() => handleDelete(id)}>
-          Delete
-        </button>
-      </div>
-    );
-  }
-}
+const EduItem = (props: EduItemProps) => {
+  const { id, defaultValues, handleDelete, onChangeHandler } = props;
+  return (
+    <div className='edu-item'>
+      <LabeledInput
+        id={id}
+        inputId='institution'
+        inputName='Institution'
+        defaultValue={defaultValues.institution}
+        onChangeHandler={onChangeHandler}
+      />
+      <LabeledInput
+        id={id}
+        inputId='degree'
+        inputName='Degree'
+        defaultValue={defaultValues.degree}
+        onChangeHandler={onChangeHandler}
+      />
+      <LabeledInput
+        id={id}
+        inputId='subject'
+        inputName='Subject'
+        defaultValue={defaultValues.subject}
+        onChangeHandler={onChangeHandler}
+      />
+      <LabeledInput
+        id={id}
+        inputId='start-date'
+        inputName='Start Date'
+        inputType='date'
+        defaultValue={defaultValues.startDate}
+        onChangeHandler={onChangeHandler}
+      />
+      <LabeledInput
+        id={id}
+        inputId='end-date'
+        inputName='End Date'
+        inputType='date'
+        defaultValue={defaultValues.endDate}
+        onChangeHandler={onChangeHandler}
+      />
+      <button
+        className='del-btn span-full-width'
+        onClick={() => handleDelete(id)}>
+        Delete
+      </button>
+    </div>
+  );
+};
 
 export default EduItem;
